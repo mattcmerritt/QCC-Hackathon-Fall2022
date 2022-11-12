d414a6569cd1af3045dd810b6a77cf282c7a5a11
mattcmerritt <mattcmerritt@gmail.com>
2022-11-12 14:26:23 -0500

    created connection over bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include <M5Core2.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include "BluetoothSerial.h"
+   
+BluetoothSerial SerialBT;
+int count = 0;
+    
+void setup()
+{
+  SerialBT.begin("Demo ESP32 Device");
+  Serial.begin(115200);
+  M5.begin();
+  M5.Lcd.clear();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+  
+// every 2 seconds, the screen flashes green and the device sends a message over bluetooth
+void loop()
+{
+  count++;
+  if (count % 20 == 0) {
+    SerialBT.println("Timer finished!");
+    M5.Lcd.fillScreen(GREEN);
+  }
+  else {
+    M5.Lcd.fillScreen(RED);
+  }
+  delay(100);
 }
