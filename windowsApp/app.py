857291a7b5bb8fbaf4748bff23c04142ab9e35f6
mattcmerritt <mattcmerritt@gmail.com>
2022-11-12 15:42:21 -0500

    reading port and activating notifications

--- windowsApp/app.py
+++ windowsApp/app.py
@@ -1,43 +1,7 @@
 import serial
-import time
 from winotify import Notification, audio
 from tkinter import *
 from tkinter import ttk
-
-personalPort = None
-# Tkinter frame instance
-root = Tk()
-# Setting geometry of frame
-root.geometry("500x250")
-
-# Getting port entry from User
-def getPort():
-    personalPort = portEntry.get()
-    print(personalPort)
-
-# Label
-ttk.Label(root, text="Input the Port")
-
-# User entry for the port
-portEntry = Entry(root, width=40)
-portEntry.focus_set()
-portEntry.pack()
-
-# Buttons
-ttk.Button(root, text="Submit", command=getPort).pack(pady=20)
-
-root.mainloop()
-
-# If port is not empty, latch onto port and read the line
-def checkPort():
-    if personalPort != None:
-        # Initializing port connection
-        serialPort = serial.Serial(port = personalPort, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-        # If available connection readLine
-        if serialPort.in_waiting > 0:
-            serialString = serialPort.readLine()
-            # If theft string, call notification
-            notification(serialString)
 
 # Setting up windows Notification
 notifyStolen = Notification(app_id = "Theft Prevention Script",
@@ -60,9 +24,49 @@ notifyDisabled.set_audio(audio.Default, loop=False)
 
 # Show windows notification
 def notification(serialString):
-    if serialString == "BEING STOLEN":
+    if "BEING STOLEN" in serialString:
         notifyStolen.show()
-    if serialString == "ALARM ACTIVE":
+    if "ALARM ACTIVE" in serialString:
         notifyActive.show()
-    if serialString == "ALARM DISABLED":
+    if "ALARM DISABLED" in serialString:
         notifyDisabled.show()
+
+# If port is not empty, latch onto port and read the line
+def checkPort(personalPort):
+    print("Attempting connection with " + str(personalPort))
+    # Initializing port connection
+    serialPort = serial.Serial(port = personalPort, baudrate=115200)
+
+    while serialPort.isOpen():
+        # If available connection readLine
+        if serialPort.in_waiting > 0:
+            line = serialPort.readline()
+            serialString = line.decode("utf-8")
+            print(serialString.strip())
+            # If theft string, call notification
+            notification(serialString)
+
+# Tkinter frame instance
+root = Tk()
+# Setting geometry of frame
+root.geometry("500x250")
+root.title("Port selection for theft alert")
+
+# Getting port entry from User
+def getPort():
+    personalPort = portEntry.get()
+    root.destroy()
+    checkPort(personalPort)
+
+# Label
+ttk.Label(root, text="Input the Port")
+
+# User entry for the port
+portEntry = Entry(root, width=40)
+portEntry.focus_set()
+portEntry.pack()
+
+# Buttons
+ttk.Button(root, text="Submit", command=getPort).pack(pady=20)
+
+root.mainloop()
